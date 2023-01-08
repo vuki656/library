4a9570114ac0313cb5affe242960ef7955fde6c1
domagoj.vukovic <off.vukovic@gmail.com>
2023-01-08 17:04:12 +0100

    feat: mantine setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { MantineProvider } from '@mantine/core';
+import Head from "next/head";
 
 const App = (props: AppProps) => {
     const { Component, pageProps } = props
 
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Library</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{ colorScheme: "light" }}
+            >
+                <Component {...pageProps} />
+            </MantineProvider>
+        </>
+    )
 }
 
 export default App
